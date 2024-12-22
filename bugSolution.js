1ef10b1d31fb8ac0c0fcba84aac7409e16b886ba
BugHunter-X <askzenai@gmail.com>
2024-12-22 11:30:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Outlet/>
+    </div>
+  );
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+```
